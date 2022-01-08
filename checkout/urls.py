cbebcb3547db552a9699df4e3c32b3bd1e391ce8
lukdav <luke-davies@outlook.com>
2022-01-08 22:22:26 +0000

    Added card elements

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,13 +1,6 @@
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.checkout, name='checkout'),
-# ]
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
 ]
