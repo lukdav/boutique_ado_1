2b5c1e50f413792c475a346b78b384b10d830396
lukdav <luke-davies@outlook.com>
2022-01-08 21:00:32 +0000

    Fixed bug on checkout views and templates

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,13 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.checkout, name='checkout'),
+# ]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
+    path('', views.checkout, name='checkout')
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
+# from django.shortcuts import render, redirect, reverse
+# from django.contrib import messages
+
+# from .forms import OrderForm
+
+
+# def checkout(request):
+#     bag = request.session.get('bag', {})
+#     if not bag:
+#         messages.error(request, "There's nothing in your bag at the moment")
+#         return redirect(reverse('products'))
+
+#     order_form = OrderForm()
+#     template = 'checkout/checkout.html'
+#     context = {
+#         order_form: 'order_form',
+#     }
+
+#     return render(request, template, context)
+
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
@@ -13,7 +33,7 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        order_form: 'order_form',
+        'order_form': order_form,
     }
 
     return render(request, template, context)
